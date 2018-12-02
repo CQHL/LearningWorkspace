0ab93f482a8d0cdd719bfdf9c0bae655e0b2bfc1
何玲 <heling7@yonyou.com>
2018-12-03 00:28:58 +0800

    具体工厂类

---
+++ src/com/pattern/sample/abstractFactory/listfactory/ListLink.java
@@ -0,0 +1,13 @@
+package com.pattern.sample.abstractFactory.listfactory;
+
+import com.pattern.sample.abstractFactory.factory.Link;
+
+public class ListLink extends Link{
+
+	public ListLink(String caption, String url){
+		super( caption, url);
+	}
+	public String makeHTML(){
+		return " <li><a href=\"" + url + "\"" + caption + "</a></li>\n";
+	}
+}
